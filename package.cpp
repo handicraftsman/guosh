318860a55041ea6ef61ad6738d85c64e4e43fb10
Nickolay Ilyushin <nickolay02@inbox.ru>
2018-09-13 14:20:41 +0300

    Oops

--- package.cpp
+++ package.cpp
@@ -4,7 +4,7 @@ using namespace PPM::Utils;
 
 PPM_PACKAGE(p) {
   p->name("guosh");
-  p->version(PPM::Version("v0.4.1"));
+  p->version(PPM::Version("v0.4.2"));
   p->description("A simple logger for C/++ ");
   p->github(PPM::GitHub("handicraftsman", "guosh"));
   p->license(PPM::License("MIT", "https://github.com/handicraftsman/guosh/blob/master/LICENSE.txt"));
